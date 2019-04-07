6a35165c766f20b988bbd9d9d53b9c7c4db397b3
Daniil Dyachenko <daniilstv@gmail.com>
2019-04-07 19:56:04 +0300

    HW for lesson 4 normal level

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -20,6 +20,10 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+import re
+
+result = re.split(r'[A-Z]+', line)
+print(result)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +49,51 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+import re
+
+result = re.findall(r'[a-z]{2,}([A-Z]{2,})[A-Z]', line_2)
+result = [i[:-1] for i in result]
+
+print(result)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+
+import os
+import random
+
+n = list(random.randint(0,9) for _ in range(2500))
+
+count = 1
+max_len = 1
+max_id = 0
+for i in range(len(n)-1):
+    y = 0
+    if n[i] == n[i+1]:
+        j = i+1
+        
+        while n[i] == n[j] or i == len(n)-1:
+            
+            
+            if count < n[j] * (j - i + 1):
+                count = n[j] * (j - i + 1)
+            if max_len < j - i+1:
+                max_len = j - i+1
+                max_id = n[i]
+                
+            #print( n[j],'len',j - i+1, 'count = ', count, "max_len =", max_len, "max_id =",max_id  )
+            
+            j += 1
+
+print(f"Самая длинная последовательность: {max_len} цифры {max_id}")
+            
+txt = "".join(map(str, n))
+            
+file = open("lesson4_n_3.txt", "w")
+file.write(txt)
+file.close()
